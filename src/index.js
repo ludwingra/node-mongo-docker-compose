88cf75c19799a2c0b946060d5c9a6b4c7888c864
Ludwing Rivera Amador <ludwingnidoo@192.168.0.3>
2020-07-19 18:10:24 -0500

    Estructura del proyecto y definición del servidor

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', 3000);
+
+app.listen(app.get('port'), () => {
+  console.log(`Listenning server on port ${app.get('port')}`);
+})
